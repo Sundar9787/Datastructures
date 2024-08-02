b05e6167b7614041cf55869646caf142a5552790
Sundar9787 <140437004+Sundar9787@users.noreply.github.com>
2024-08-02 22:37:21 +0530

    Update Merging array without duplicates.py

--- Array/Merging array without duplicates.py
+++ Array/Merging array without duplicates.py
@@ -1,1 +1,10 @@
+arr_1=[1,2,4,5,6]
+arr_2=[2,3,5,7,8]
+seen = arr_1
+def merge_array(arr_1,arr_2):
+    for element  in arr_2:
+        if element not in seen:
+            arr_1.append(element)
+    return sorted(arr_1)
 
+print(merge_array(arr_1,arr_2))
