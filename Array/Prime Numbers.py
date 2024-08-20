360482b078861030e1f06219b9c5e1e3aebfe193
Sundar9787 <140437004+Sundar9787@users.noreply.github.com>
2024-08-20 10:13:56 +0530

    Create Prime Numbers.py

---
+++ Array/Prime Numbers.py
@@ -0,0 +1,10 @@
+N = int(input("Enter the number : "))
+if N < 0:
+    print('It is not possible for negative number')
+    
+if N % 2!= 0 and N%3!=0 and N%5 !=0:
+    print("It is a prime number")
+    
+else:
+    print('It is not a Prime Number')
+       
